52020507f24c2a84d667f509ebb6afd52dc7fed6
T-Y89 <0809taikiyamashita@gmail.com>
2023-02-14 22:12:36 +0900

    2-5

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Homes', type: :request do
-  describe 'GET /top' do
-    it 'returns http success' do
-      get '/home/top'
-      expect(response).to have_http_status(:success)
+  describe '/にアクセスした時に' do
+    it 'HTTPステータスが200を返すこと' do
+      get '/'
+      expect(response).to have_http_status(200)
     end
   end
 end
